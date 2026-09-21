72ed0a073f629354f53b7c5ae397e8b57f53c617
meera-digi <meera.gowdu@digidense.com>
2026-09-21 18:00:37 +0530

    JIRA-10: Add VPC

--- main.tf
+++ main.tf
@@ -1,9 +1,16 @@
+#aws_instance creation
+
 resource "aws_instance" "example" {
-  ami           = "ami-123456"
-  instance_type = "t2.micro"
+ami            = "ami-123456"
+instance_type  = "t2.micro"
 
-  tags = {
-    Name = "new-name"
-  }
+tags  ={
+   Name = "new-name"
+ }
+}
 
+# creating aws_vpc
+
+resource "aws_vpc" "lab" {
+  cidr_block = "10.0.0.0/16"
 }
