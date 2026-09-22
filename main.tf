c72c5572505646ecad96889719290ae1eead7660
meera-digi <meera.gowdu@digidense.com>
2026-09-22 10:48:49 +0530

    JIRA-10: Add VPC

--- main.tf
+++ main.tf
@@ -9,24 +9,8 @@ tags  ={
  }
 }
 
-# creating aws_vpc
+# creating aws_vpc 
 
 resource "aws_vpc" "lab" {
   cidr_block = "10.0.0.0/16"
-
 }
-
-# creating aws_subnet
-
-resource "aws_subnet" "lab" {
-  vpc_id     = aws_vpc.lab.id
-  cidr_block = "10.0.1.0/24"
-}
-
-# creating aws_security_group
-
-resource "aws_security_group" "lab" {
-  name   = "lab-sg"
-  vpc_id = aws_vpc.lab.id
-}
-
