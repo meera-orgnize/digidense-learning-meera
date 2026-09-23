4c260f2718a71ce47fe36138cdbc329d7058f8a6
meera-digi <meera.gowdu@digidense.com>
2026-09-23 11:09:07 +0530

    JIRA-11: Add subnet

--- main.tf
+++ main.tf
@@ -16,3 +16,9 @@ resource "aws_vpc" "lab" {
   cidr_block = "10.0.0.0/16"
 }
 
+# creating aws_subnet
+
+resource "aws_subnet" "lab" {
+  vpc_id     = aws_vpc.lab.id
+  cidr_block = "10.0.1.0/24"
+}
